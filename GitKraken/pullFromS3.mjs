c152735f073acf0f5372e30a1831be93017abb72
John Alden <john.alden@gitkraken.com>
2025-05-28 16:46:13 -0700

    esm-ify pullFromS3

--- GitKraken/pullFromS3.mjs
+++ GitKraken/pullFromS3.mjs
@@ -1,24 +1,29 @@
-const fs = require("fs");
-const fse = require("fs-extra");
-const path = require("path");
-const got = require("got");
-const stream = require("stream")
-const util = require("util");
+import fs from "fs"
+import fse from "fs-extra"
+import path from "path"
+import got from "got"
+import stream from "stream"
+import util from "util"
+
+import { getDistNames } from "./configHelper.js";
+
+import gkPkgJson from './package.json' with { type: "json" };
+import rootPkgJson from '../package.json' with { type: "json" };
+import rebuildConfig from "./rebuild_docker_config.json" with { type: "json" };
+
+const { version } = rootPkgJson;
+const { gitkrakenPrebuilts: { bucketName } } = gkPkgJson;
 
 const pipeline = util.promisify(stream.pipeline);
 
-const { gitkrakenPrebuilts: { bucketName } } = require('./package.json');
-const { version } = require("../package.json");
-const rebuildConfig = require("./rebuild_docker_config.json");
-const { getDistNames } = require("./configHelper");
-
-const binaryDir = path.resolve(__dirname, "additional-binaries");
-const buildReleaseDir = path.resolve(__dirname, "..", "build", "Release");
+const binaryDir = path.resolve(import.meta.dirname, "additional-binaries");
+const buildReleaseDir = path.resolve(import.meta.dirname, "..", "build", "Release");
 
 const getBinaryName = (distName, version) => `nodegit-${version}-${distName}.node`;
 const getFriendlyBinaryName = distName => `nodegit-${distName}.node`;
 
 const downloadBinaryFromS3 = async binaryName => {
+  console.log(`Downloading https://${bucketName}.s3.amazonaws.com/${binaryName}`);
   await pipeline(
     got.stream(`https://${bucketName}.s3.amazonaws.com/${binaryName}`),
     fs.createWriteStream(path.resolve(binaryDir, binaryName))
@@ -51,18 +56,19 @@ const cleanup = async () => {
   await fse.remove(path.join(__dirname, 'node_modules'));
 };
 
-const acquireBinariesFromS3 = async () => {
+export const acquireBinariesFromS3 = async () => {
   await fse.ensureDir(binaryDir);
   await downloadAllBinaries();
   await copyBinaries();
   await cleanup();
 };
 
-module.exports = acquireBinariesFromS3;
-
-if (require.main === module) {
-  module.exports().catch((error) => {
+if (process.argv[1] === import.meta.filename) {
+  try {
+    await acquireBinariesFromS3();
+  }
+  catch(error) {
     console.error('Pull from S3 failed: ', error);
     process.exit(1);
-  });
+  };
 }
